7bc8af653efe7719caa10d19cc922785e0a07e0e
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-20 16:01:30 -0400

    initial sketch of the component

---
+++ Component.js
@@ -0,0 +1,10 @@
+export deafault function Component({ propCount }) {
+  const count = 0
+  const propCountDoubled = 0
+  
+  return `
+    State: ${count}
+    Prop: ${propCount}
+    Prop Doubled: ${propCountDoubled}
+  `
+}
