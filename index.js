8f6576d844824a0531cc0d33f17b983f4f74af4f
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-21 00:43:37 -0400

    annotated the btn-prop eventlistener

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { render } from './MyReact.js' // import the render function from MyReact
 import Component from './Component.js' // imports Component from Component
 
 let propCount = 0 // sets initial prop value
+
+// adds the btn-prop click function that tally's up the propcount with each click
 document.getElementById("btn-prop").addEventListener("click", () => {
   propCount++
   renderComponent()
