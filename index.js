3c4b419859b39b95d359981a37dc1037d0745f9b
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-21 00:41:39 -0400

    Added the btn-prop eventlistener function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-import { render } from './MyReact.js'
-import { Component } from './Component.js'
+import { render } from './MyReact.js' // import the render function from MyReact
+import Component from './Component.js' // imports Component from Component
+
+let propCount = 0 // sets initial prop value
+document.getElementById("btn-prop").addEventListener("click", () => {
+  propCount++
+  renderComponent()
+})
 
 function renderComponent() {
-  render(Component), {}
+  render(
+    Component, // renders the component
+    { propCount }, // sets the initial prop count
+    document.getElementById("root") // parent
+  )
 }
+
+renderComponent()
